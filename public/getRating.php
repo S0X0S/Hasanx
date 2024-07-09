21583fcd881e085f7d71566731e6dcbed24e3a27
Hasan Korkmaz <hasanlarx50@gmail.com>
2024-07-09 12:09:22 +0300

    s

--- public/getRating.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-Type: application/json; charset=utf-8');
-    header('Access-Control-Allow-Origin: https://s0x0s.github.io/Hasanx/');
-    $con = mysqli_connect("localhost", "root", "", "portfolio");
-    $query = "SELECT AVG(`vote`) AS `avg` FROM `rating`";
-    $res = mysqli_query($con, $query);
-    echo "{avg:".round(mysqli_fetch_assoc($res)['avg'],1)."}";
-?>
