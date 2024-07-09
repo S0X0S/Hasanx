d309fd64f3a21d65c528272a1a9924eb71c4b2b4
Hasan Korkmaz <hasanlarx50@gmail.com>
2024-07-09 12:04:13 +0300

    Update setRating.php

--- public/setRating.php
+++ public/setRating.php
@@ -4,7 +4,7 @@
     if(isset($_POST["value"])) {
         $value = $_POST["value"];
 
-        $con = mysqli_connect("localhost", "root", "", "portfolio");
+        $con = mysqli_connect("localhost", "root", "", "testing");
         
         $query = "INSERT INTO `rating` (`Vote`) VALUES ('$value')";
         $res = mysqli_query($con, $query);
